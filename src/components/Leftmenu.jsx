6a6af0753622bc5e7be4c63de140769a6809d3c1
mohamedelleuch777 <usher7med@gmail.com>
2025-02-08 22:23:32 +0100

    Leftmenu done

--- src/components/Leftmenu.jsx
+++ src/components/Leftmenu.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { List, PersonFill, BellFill } from 'react-bootstrap-icons'; // Import individual icons
+import { HouseFill, PersonFill, BellFill, ListTask } from 'react-bootstrap-icons'; // Import individual icons
+import logo from '../assets/img/logo.svg';
+
+const menuItems = [
+  { id: 1, name: 'Home', icon: <HouseFill color="white" size={20} /> },
+  { id: 2, name: 'Profile', icon: <PersonFill color="white" size={20} /> },
+  { id: 3, name: 'Notifications', icon: <BellFill color="white" size={20} /> },
+  { id: 4, name: 'My Task', icon: <ListTask color="white" size={20} /> }
+];
 
 function Leftmenu({ isMenuOpen, setIsMenuOpen }) {
 
@@ -7,7 +15,22 @@ function Leftmenu({ isMenuOpen, setIsMenuOpen }) {
     <>
       <div className={`leftmenu-overlay ${isMenuOpen ? 'show' : ''}`} onClick={() => setIsMenuOpen(false)}></div>
       <div className={`leftmenu leftmenu-containter ${isMenuOpen ? 'show' : ''}`}>
-        
+        <div className="title-container">
+          <i className="app-name">ModaSync v1.0.0</i>
+          <img className="logo" src={logo} alt="logo" />
+        </div>
+        {
+          menuItems && menuItems.map((item) => (
+            <div className="menu-item" key={item.id}>
+              <div className="menu-item-icon">
+                {item.icon}
+              </div>
+              <div className="menu-item-name">
+                {item.name}
+              </div>
+            </div>
+          ))
+        }
       </div>
     </>
   );
