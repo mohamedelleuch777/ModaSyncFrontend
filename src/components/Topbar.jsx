5e9c2815ada1caac123e81d6591fdde013f1a095
mohamedelleuch777 <usher7med@gmail.com>
2025-02-08 21:22:15 +0100

    few css ajustements

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,14 +5,14 @@ function Topbar({ setIsMenuOpen }) {
   return (
     <div className="topbar topbar-containter">
       <div className="button menu" onClick={() => setIsMenuOpen(true)}>
-        <List color="white" size={30} /> {/* Render a blue arrow-right icon */}
+        <List color="white" size={20} /> {/* Render a blue arrow-right icon */}
       </div>
       <div className="buttons-container">
         <div className="button menu">
-            <BellFill color="white" size={30} /> {/* Render a blue arrow-right icon */}
+            <BellFill color="white" size={20} /> {/* Render a blue arrow-right icon */}
         </div>
         <div className="button menu">
-            <PersonFill color="white" size={30} /> {/* Render a blue arrow-right icon */}
+            <PersonFill color="white" size={20} /> {/* Render a blue arrow-right icon */}
         </div>
       </div>
     </div>
