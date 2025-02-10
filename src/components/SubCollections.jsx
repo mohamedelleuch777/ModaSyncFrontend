777816bf9e769da0fa130a8176c2170092173b90
mohamedelleuch777 <usher7med@gmail.com>
2025-02-10 10:36:48 +0100

    add sample's count for each subcollection

--- src/components/SubCollections.jsx
+++ src/components/SubCollections.jsx
@@ -3,17 +3,6 @@ import { get, useApi } from '../hooks/apiHooks';
 import { BinocularsFill } from 'react-bootstrap-icons'
 import LoadingSpinner from './LoadingSpinner';
 
-
-
-const stories = [
-  { id: 1, name: 'Collection Eté 2024', image: 'https://i.pinimg.com/736x/8b/84/2d/8b842da000f098e8d6711d215679e51f.jpg' },
-  { id: 2, name: 'Collection Hiver 2025', image: 'https://i.pinimg.com/736x/32/0a/5e/320a5eb3ffc4af1b500df89e798d1810.jpg' },
-  { id: 3, name: 'Collection Aid S 2025', image: 'https://i.pinimg.com/736x/8b/84/2d/8b842da000f098e8d6711d215679e51f.jpg' },
-  { id: 4, name: 'Collection Printemps 2025', image: 'https://i.pinimg.com/736x/73/89/f2/7389f2ba86f53d464fe67381daf6dead.jpg' },
-  { id: 5, name: 'Collection Aid G 2025', image: 'https://i.pinimg.com/736x/8b/84/2d/8b842da000f098e8d6711d215679e51f.jpg' },
-  { id: 6, name: 'Collection summerß 2025', image: 'https://i.pinimg.com/736x/73/89/f2/7389f2ba86f53d464fe67381daf6dead.jpg' },
-];
-
 function SubCollections({ selectedCollectionId }) {
   const [isLoading, setIsLoading] = React.useState(true);
   const [subCollections, setSubCollections] = React.useState([]);
@@ -38,10 +27,13 @@ function SubCollections({ selectedCollectionId }) {
         isLoading ? <LoadingSpinner /> : (
           <>
             {
-              subCollections && subCollections.map((story) => (
-                <div className="sub-collection" key={story.id}>
-                  <img className='sub-collection-image' src={story.image} alt={story.name} />
-                  <p className='sub-collection-label'>{story.name}</p>
+              subCollections && subCollections.map((subCollection) => (
+                <div className="sub-collection" key={subCollection.id}>
+                  <img className='sub-collection-image' src={subCollection.image} alt={subCollection.name} />
+                  <div>
+                    <p className='sub-collection-label'>Name: {subCollection.name}</p>
+                    <p className='samples-count-label'>Samples Count: {subCollection.count}</p>
+                  </div>
                 </div>
               ))
             }
