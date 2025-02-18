982d9e4d17e59d10403b205f117b4afb60321e82
mohamedelleuch777 <usher7med@gmail.com>
2025-02-18 09:15:14 +0100

    adding new feature: auto close slider wrapper when 10 sec of idle

--- src/components/ButtonSliderWrapper.jsx
+++ src/components/ButtonSliderWrapper.jsx
@@ -4,9 +4,15 @@ function ButtonSliderWrapper({ children }) {
   const [scrollValue, setScrollValue] = React.useState(0);
   const [maxScrollHorizontalValue, setMaxScrollHorizontalValue] = React.useState(0);
   const divWrapper = React.useRef(null);
+  const [countingInterval, setCountingInterval] = React.useState(null);
 
 
   const handleScroll = (e) => {
+    if (countingInterval) {
+      clearInterval(countingInterval);
+      setCountingInterval(null);
+      console.log('clearing interval');
+    }
     const { scrollLeft, scrollWidth, clientWidth } = e.target;
     setMaxScrollHorizontalValue(scrollWidth - clientWidth)
     setScrollValue(scrollLeft);
@@ -23,6 +29,18 @@ function ButtonSliderWrapper({ children }) {
     requestAnimationFrame(() => {
       div.scrollTo({ left: fixScroll, behavior: "smooth" });
     });
+    
+    // wait 100 ms to end the scroll event and set the interval
+    setTimeout(() => {
+      const interval = setTimeout(() => {
+        fixScroll = 0
+        requestAnimationFrame(() => {
+          div.scrollTo({ left: fixScroll, behavior: "smooth" });
+        });
+      }, 10000); // force close if stay opened and IDLE for 10 seconds
+      setCountingInterval(interval);
+      console.log('setting interval');
+    }, 1000)
   };
 
   return (
